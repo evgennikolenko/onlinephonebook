ae776c1af4d651c45ceca9fd655768621daab9b0
Evgen Nikolenko <evgennikolenko@ukr.net>
2018-02-12 22:57:48 +0200

    create service with func for database

--- src/js/app/service/phonebookDatebaseService.js
+++ src/js/app/service/phonebookDatebaseService.js
@@ -1,3 +1,16 @@
 /**
  * Created by evgen on 12.02.18.
  */
+phonebookDatebaseService
+
+angular.module('phoneBookApp')
+    .service('phonebookDatebaseService', phonebookDatebaseService);
+
+function phonebookDatebaseService($state, $rootScope) {
+    // Get a reference to the database service
+    var database = firebase.database();
+
+    this.createUserRoom = function (uid) {
+        database.ref('phonesbook/authUsers/' + uid).set(1);
+    }
+}
