b0482953d291e58d181f07fa5573e4fb3b4e90f6
Evgen Nikolenko <evgennikolenko@ukr.net>
2018-02-19 22:36:17 +0200

    add delete user-card;design userCard;

--- src/js/app/directive/userCard/user-card.js
+++ src/js/app/directive/userCard/user-card.js
@@ -1,19 +1,32 @@
+
 /**
  * Created by evgen on 14.02.18.
  */
 angular.module('phoneBookApp')
-    .directive('userCard', function () {
+    .directive('userCard', function ($timeout) {
         return {
             restrict: 'E',
             transclude: true,
             templateUrl: 'js/app/directive/userCard/user-card.html',
+            // scope : {
+            //     onCancel: '&',
+            //     list: '@'
+            // },
+            replace: true,
             link: function(scope, element, attrs, controller) {
-                // console.log('cc', controller);
-                // console.log('ss', attrs );
+
+                scope.onDeleteCard = function () {
+                    element.fadeOut();
+
+                    $timeout( function(){
+                        var uid = firebase.auth().currentUser.uid;
+                        var cardUid = scope.list.$id;
+                        firebase.database().ref('phonesbook/authUsers/' + uid + '/userCard/' + cardUid).remove();
+                    }, 1000 );
+                };
             },
             bindToController: true,
             controller: function () {
-
             },
             controllerAs: 'userCard'
         }
