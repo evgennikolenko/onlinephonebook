542f21a9c2a57cff8c89ee141c1fd186e7666dc3
Evgen Nikolenko <evgennikolenko@ukr.net>
2018-02-24 22:48:02 +0200

    add func openModal, comments. In view delete buttons and forms

--- src/controllers/phonebook-controller/phonebook.js
+++ src/controllers/phonebook-controller/phonebook.js
@@ -9,51 +9,74 @@ angular.module('phoneBookApp.phonebook', ['ui.router']);
 angular.module('phoneBookApp.phonebook')
     .config(Config)
     .controller('phonebookCtrl', phonebookCtrl);
+
 function phonebookCtrl(phonebookDatebaseService) {
+
     var phonebook = this;
 
-    phonebook.showModal = false;
-    phonebook.showModalAddCard =function () {
-        phonebook.showModal = true;
-    };
+    /*
+    * Function start when the button 'openModal' is pressed;
+    * 'avatarColor' choose a color for avatar;
+     */
+   phonebook.openModal = function () {
+        phonebook.avatarColor = function () {
+           var a = ['b2e2a2', '53b1f0', '6fb1e4', 'ce90e2', 'ecd074', '73cdd0'];
+           return a[Math.floor(Math.random() * a.length)];
+         };
+       phonebook.avatar = phonebook.avatarColor();
 
-    phonebook.objUserAdd = {
-        firstname: '',
-        lastname: '',
-        address: '',
-        email: '',
-        phone1: '',
-        phone2: ''
-    };
+       /*
+       * Object with settings user-card for firebase;
+       * 'objUserAdd' is filled from the form in modal window
+       */
+       phonebook.objUserAdd = {
+           firstname: '',
+           lastname: '',
+           address: '',
+           email: '',
+           phone1: '',
+           phone2: '',
+           avatar: phonebook.avatar
+       };
+   };
 
+    /*
+     * Function sends info about user in firebase and create a card;
+     * 'phonebookDatebaseService' - service for work with database;
+     */
     phonebook.addUserCard = function () {
         phonebookDatebaseService.addUserCard(phonebook.objUserAdd);
-
-        phonebook.showModal = false;
     };
+
+    /*
+     *  Clear the form after created user
+     */
     phonebook.closeModalWindow = function () {
         $(".modal-body form input").val("");
     };
 
-        phonebookDatebaseService.loadUserCard().then(function (userCard) {
+    /*
+     * Load data from database - 'loadUserCard()';
+     * 'phonebookDatebaseService' - service for work with database;
+     * 'phonebook.list' - model (user cards);
+     * 'phonebook.myData' - info about me;
+     */
+    phonebookDatebaseService.loadUserCard().then(function (userCard) {
             phonebook.list = userCard;
-            console.log("TT", phonebook.list);
             phonebook.myData = phonebook.list.pop();
-            console.log("TTx", phonebook.myData);
         });
-
 }
 
 
-Config.$inject = ['$stateProvider', '$urlRouterProvider'];
 
-function Config($stateProvider, $urlRouterProvider) {
+Config.$inject = ['$stateProvider'];
+
+function Config($stateProvider) {
     $stateProvider.state('userroom', {
         url: "/phonebook/home/userroom",
         templateUrl: 'controllers/phonebook-controller/phonebook.html',
         resolve: {
             auth: function ($q, $state, loginService) {
-                console.log('RRR', loginService.getUser());
                 if (loginService.getUser() === null) {
                     $q.reject();
                     alert('Вы должны авторизироваться!');
