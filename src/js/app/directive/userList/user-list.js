43b7c81b94deb69d9cd0d7609abe42a54fa0abe8
Evgen Nikolenko <evgennikolenko@ukr.net>
2018-02-26 20:18:28 +0200

    create directive user-list (display users in list)

--- src/js/app/directive/userList/user-list.js
+++ src/js/app/directive/userList/user-list.js
@@ -1,3 +1,30 @@
 /**
  * Created by evgen on 26.02.18.
  */
+
+angular.module('phoneBookApp')
+    .directive('userList', function ($timeout) {
+        return {
+            restrict: 'E',
+            transclude: true,
+            templateUrl: 'js/app/directive/userList/user-list.html',
+
+            // todo: repeat link with user-card.js (directive)
+
+            replace: true,
+            link: function(scope, element, userCard) {
+                scope.onDeleteCard = function () {
+                    element.fadeOut();
+
+                    $timeout( function(){
+                        var uid = firebase.auth().currentUser.uid;
+                        var cardUid = scope.list.$id;
+                        firebase.database().ref('phonesbook/authUsers/' + uid + '/userCard/' + cardUid).remove();
+                    }, 1000 );
+                };
+            },
+            bindToController: true,
+            controller: function () {},
+            controllerAs: 'userList'
+        }
+    });
